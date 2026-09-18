e6fd75d3ee8290b1dd95a439b66c91b67d10c86f
Cameron-Wells8 <cameron.wells1@gmail.com>
2026-09-18 09:24:40 -0400

    fix other type in calc target score

--- targetscore/R/calc_target_score.R
+++ targetscore/R/calc_target_score.R
@@ -129,7 +129,7 @@ calc_target_score <- function(wk, wks, dist_ind, edgelist, n_dose, n_prot, prote
           tsp_down[i, node1, node2] <- ts_pathway_scale * (2^-(dist_ind[node1, node2])) * ifelse(abs(,proteomic_responses[i, node2]) < neighbor_sign_tolerance, 0, sign(response_value)) * wk[node1, node2]
           
         }else{
-          tsp_up[i, node1, node2] <- ts_pathway_scale * (2^-(dist_ind[node1, node2])) * ,proteomic_responses[i, node1] * wk[node1, node2]
+          tsp_up[i, node1, node2] <- ts_pathway_scale * (2^-(dist_ind[node1, node2])) * proteomic_responses[i, node1] * wk[node1, node2]
           tsp_down[i, node1, node2] <- ts_pathway_scale * (2^-(dist_ind[node1, node2])) * proteomic_responses[i, node2] * wk[node1, node2]
           
         }
